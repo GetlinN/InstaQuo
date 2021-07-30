922d614c7c7dd9239e09b1fc864c3e1f4f6fa0df
Nataliia Getlin <natali.getlin@gmail.com>
2021-07-30 11:20:15 -0700

    Project initiated

---
+++ InstaQuoClient/InstaQuoClient/InstaQuoClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaQuoClientApp.swift
+//  InstaQuoClient
+//
+//  Created by Nataliia Getlin on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct InstaQuoClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
